541af57a969adcba65163c57bd962d75074eee8b
bren0r <nuriomonov1@mail.ru>
2023-10-08 15:09:40 +0300

    добавлена вьюшка для нумерации страниц

--- backend/modules/blog/views.py
+++ backend/modules/blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DeleteView
 
-from .models import Article
+from .models import Article, Category
+
 
 class ArticleListView(ListView):
     model = Article
@@ -23,3 +24,32 @@ class ArticleDetailView(DeleteView):
         context['title'] = self.object.title
         return context
 
+
+class ArticleByCategoryListView(ListView):
+    model = Article
+    template_name = 'blog/articles_list.html'
+    context_object_name = 'articles'
+    category = None
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Article.objects.all().filter(category__slug=self.category.slug)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Статья из категории: {self.category.title}'
+        return context
+
+
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+
+def articles_list(request, page):
+    articles = Article.objects.all()
+    paginator = Paginator(articles, per_page=2)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+    context = {'page_obj': page_object}
+    return render(request, 'blog/articles_func_list.html', context)
