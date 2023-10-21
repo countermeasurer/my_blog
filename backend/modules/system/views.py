29cf6c7a6d9d6504fe484c415fcea394350cad8e
bren0r <nuriomonov1@mail.ru>
2023-10-21 15:53:07 +0300

    добавлено представление для регистрации,авторизации, выхода

--- backend/modules/system/views.py
+++ backend/modules/system/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, CreateView
 from django.db import transaction
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .models import Profile
-from .forms import UserUpdateForm, ProfileUpdateForm
+from .forms import UserUpdateForm, ProfileUpdateForm, UserRegisterForm, UserLoginForm
 
 
 class ProfileDetailView(DetailView):
@@ -55,3 +57,41 @@ class ProfileUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('profile_detail', kwargs={'slug': self.object.slug})
+
+
+class UserRegisterView(SuccessMessageMixin, CreateView):
+    """
+    Представление регистрации на сайте с формой регистрации
+    """
+
+    form_class = UserRegisterForm
+    success_url = reverse_lazy
+    template_name = 'system/user_register.html'
+    success_message = 'Вы успешно зарегистрировались. Можете авторизоваться на сайте!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация на сайте'
+        return context
+
+
+class UserLoginView(LoginView):
+    """
+    Автоирзация на сайте
+    """
+    form_class = UserLoginForm
+    template_name = 'system/user_login.html'
+    next_page = 'home'
+    success_url = 'Добро пожаловать в систему!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Авторизация в системе'
+        return context
+
+
+class UserLogoutView(LogoutView):
+    """
+    Выход из системы
+    """
+    next_page = 'home'
