5e473e6901128210e8c1b6c92aff8552c860a221
bren0r <nuriomonov1@mail.ru>
2023-11-02 13:41:07 +0300

    добавлена модель обратной связи в админке

--- backend/modules/system/admin.py
+++ backend/modules/system/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Feedback
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
@@ -8,3 +8,12 @@ class ProfileAdmin(admin.ModelAdmin):
     """
     list_display = ('user', 'birth_date', 'slug')
     list_display_links = ('user', 'slug')
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели обратной связи
+    """
+    list_display = ('email', 'ip_address', 'user')
+    list_display_links = ('email', 'ip_address')
