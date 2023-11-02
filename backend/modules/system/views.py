d0dfd5951a3f8eb6adf0c2cfce3f2cf8f8cb01b2
bren0r <nuriomonov1@mail.ru>
2023-11-02 13:15:16 +0300

    добавлено представление обратной связи

--- backend/modules/system/views.py
+++ backend/modules/system/views.py
@@ -223,6 +223,34 @@ class EmailConfirmationFailedView(TemplateView):
 
 
 
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import FeedbackCreateForm
+from .models import Feedback
+from ..services.email import send_contact_email_message
+from ..services.utils import get_client_ip
+
+
+class FeedbackCreateView(SuccessMessageMixin, CreateView):
+    model = Feedback
+    form_class = FeedbackCreateForm
+    success_message = 'Ваше письмо успешно отправлено администрации сайта'
+    template_name = 'system/feedback.html'
+    extra_context = {'title': 'КОнтактная информация'}
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.ip_address = get_client_ip(self.request)
+            if self.request.user.is_authenticated:
+                feedback.user = self.request.user
+            send_contact_email_message(feedback.subject, feedback.email, feedback.content, feedback.ip_address, feedback.user_id)
+        return super().form_valid(form)
+
+
+
 
 
 
