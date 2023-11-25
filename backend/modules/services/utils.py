a08c5d2b374e062c7a10edad484c59bc968dd513
bren0r <nuriomonov1@mail.ru>
2023-11-25 19:06:54 +0300

    optimize images

--- backend/modules/services/utils.py
+++ backend/modules/services/utils.py
@@ -46,3 +46,21 @@ class CkeditorCustomStorage(FileSystemStorage):
 
     location = os.path.join(settings.MEDIA_ROOT, 'uploads/')
     base_url = urljoin(settings.MEDIA_URL, 'uploads/')
+
+
+from PIL import Image, ImageOps
+
+
+def image_compress(image_path, height, width):
+    """
+    Оптимизация изображений
+    """
+    img = Image.open(image_path)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    if img.height > height or img.width > width:
+        output_size = (height, width)
+        img.thumbnail(output_size)
+    img = ImageOps.exif_transpose(img)
+    img.save(image_path, format='JPEG', quality=90, optimize=True)
+
